3f73bd4fcae965405e6efbce3b1c32f4e2619d2c
Painfu11y <333vahan777@gmail.com>
2026-02-24 18:40:16 +0400

    feat: implement TraineeService,and implementation

---
+++ src/main/java/com/gym_project/service/TraineeService.java
@@ -0,0 +1,24 @@
+package com.gym_project.service;
+
+import com.gym_project.dto.create.TraineeCreateDto;
+import com.gym_project.dto.response.TraineeResponseDto;
+import com.gym_project.dto.update.TraineeUpdateDto;
+
+import java.util.List;
+
+public interface TraineeService {
+
+    TraineeResponseDto create(TraineeCreateDto dto);
+
+    TraineeResponseDto getByUsername(String username);
+
+    List<TraineeResponseDto> getAll();
+
+    TraineeResponseDto update(String username, TraineeUpdateDto dto);
+
+    void deleteByUsername(String username);
+
+    TraineeResponseDto activate(String username);
+
+    TraineeResponseDto deactivate(String username);
+}
