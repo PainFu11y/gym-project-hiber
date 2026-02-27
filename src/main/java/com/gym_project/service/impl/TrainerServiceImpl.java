9ab73a64677ab683b7137bae034d8bbc932ce6b4
Painfu11y <333vahan777@gmail.com>
2026-02-27 19:02:31 +0400

    fix: add password generation feature in the creating method

--- src/main/java/com/gym_project/service/impl/TrainerServiceImpl.java
+++ src/main/java/com/gym_project/service/impl/TrainerServiceImpl.java
@@ -14,6 +14,7 @@ import com.gym_project.mapper.TrainingMapper;
 import com.gym_project.repository.TraineeRepository;
 import com.gym_project.repository.TrainerRepository;
 import com.gym_project.service.TrainerService;
+import com.gym_project.utils.PasswordGenerator;
 import com.gym_project.utils.UsernameGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -41,6 +42,7 @@ public class TrainerServiceImpl implements TrainerService {
 
         Trainer trainer = TrainerMapper.toEntity(dto);
         trainer.setUsername(generatedUsername);
+        trainer.setPassword(PasswordGenerator.generate());
 
         trainerRepository.save(trainer);
 
